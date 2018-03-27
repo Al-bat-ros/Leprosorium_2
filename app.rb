1932def905e069d11757df50a0b1a72225a9c083
Al-bat-ros <al-bat-ros@mail.ru>
2018-03-27 15:55:43 +0500

    output info about the post and validation

--- app.rb
+++ app.rb
@@ -7,9 +7,15 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:leprosorium.db"
 
 class Post < ActiveRecord::Base
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :content, presence: true, length: { minimum: 1 }
 end
 
 class Comment < ActiveRecord::Base
+end
+
+before do
+  @posts = Post.all
 end
 
 get '/' do
@@ -23,5 +29,15 @@ end
 
 post '/new' do
    @c = Post.new params[:post]
-   @c.save
+
+  if @c.save
+    erb "<h1>Ваш пост опубликован</h1>"
+  else
+    @error = @c.errors.full_messages.first
+    erb :new
+  end
+end
+
+get '/details/:id' do
+  erb :details
 end
